3e336f2e6e48d9cc2f95153fd5e7305c4f5ac1d0
Samuel Atilano <samuel4atilano@icloud.com>
2026-05-21 01:17:12 -0400

    Added about page structure

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ async function runInstructSequence() {
   addCommandLink("/projects");
   addCommandLink("/contact");
   addCommandLink("/clear");
+  addCommandLink("/help");
   await typeLog("---------------");
   enableInput();
 }
@@ -133,15 +134,9 @@ async function runInstructSequence() {
 /*     Window Screen Logic      */
 /*==============================*/
 
-
-
-
 function focusWindow(win) {
   win.style.zIndex = zIndexCounter++;
 }
-
-
-
 
 function createWindow(id, title, content) {
   if (openWindows[id]) {
@@ -163,8 +158,6 @@ function createWindow(id, title, content) {
     </div>
   `;
 
-  const header = win.querySelector(".window-header");
-
   openWindows[id] = win;
 
   win.addEventListener("mousedown", () => {
@@ -178,21 +171,26 @@ function createWindow(id, title, content) {
 
   // DRAG
   let isDragging = false;
-  let offsetX = 0;
-  let offsetY = 0;
+  let startX = 0;
+  let startY = 0;
+  let currentX = 120;
+  let currentY = 120;
 
-  header.addEventListener("mousedown", (e) => {
+
+  win.addEventListener("mousedown", (e) => {
     isDragging = true;
-    offsetX = e.clientX - win.offsetLeft;
-    offsetY = e.clientY - win.offsetTop;
+    startX = e.clientX - currentX;
+    startY = e.clientY - currentY;
     document.body.classList.add("no-select");
   });
 
   document.addEventListener("mousemove", (e) => {
-    if (isDragging) {
-      win.style.left = e.clientX - offsetX + "px";
-      win.style.top = e.clientY - offsetY + "px";
+    if (!isDragging) {
+      return;
     }
+    currentX = e.clientX - startX;
+    currentY = e.clientY - startY;
+    win.style.transform = `translate(${currentX}px, ${currentY}px)`;
   });
 
   document.addEventListener("mouseup", () => {
@@ -208,29 +206,17 @@ function createWindow(id, title, content) {
 }
 
 /* ==============================*/
-/* Project Data                  */
-/* ==============================*/
-
-const projects = {
-  "dream_terminal(placeholder)": {
-    title: "Dream Terminal Portfolio",
-    description: "Terminal Style Portfolio UI",
-    problem: "Traditional Portfolios lack interactivity",
-    approach: "built command-driven HUD interface",
-    results: "Created dynamic terminal system",
-    improvements: "Add persistence animations",
-    tech: ["HTML", "CSS", "JavaScript"],
-    learnings: "UI Architecture, command parsing, UX Systems"
-  }
-};
-
-/* ==============================*/
-/* Command Logic                  */
+/* Command Logic for Links       */
 /* ==============================*/
 
 async function handleCommand(cmd) {
   let command = cmd.toLowerCase().trim();
 
+  // store history
+  history.unshift(command);
+  historyIndex = -1;
+
+  //Handles both types of links
   if (command.startsWith("/")) {
     command = command.slice(1);
   }
@@ -243,27 +229,26 @@ async function handleCommand(cmd) {
   const base = parts[0];
   const arg = parts[1];
 
-  if (command === "") return;
 
-  // store history
-  history.unshift(command);
-  historyIndex = -1;
+  //Start of Command Logic
+  await typeLog("> " + command); //Runs command animation first
 
-  await typeLog("> " + command);
 
-  if (command === "help") {
-    await typeLog("about");
-    await typeLog("projects");
-    await typeLog("open [project]");
-    await typeLog("clear");
+  if (command === "") return; //Empty input
+
+  //About command logic
+  else if (command === "about") {
+    createWindow("about", "About", renderAbout(aboutData));
   }
 
-  else if (command === "about") {
-    createWindow("about", "About", `
+  //Skills command Logic
+  else if (command === "skills") {
+    createWindow("skills", "Skills", `
       <p>I build AI-driven interfaces and systems.</p>
     `);
   }
 
+  //Project Command Logic
   else if (command === "projects") {
     await typeLog("available projects:");
     Object.keys(projects).forEach((key) => {
@@ -288,15 +273,38 @@ async function handleCommand(cmd) {
     createWindow(arg, project.title, renderProject(project));
   }
 
+  //Contact Command Logic
+  else if (command === "contact") {
+    createWindow("contact", "Contact", `
+      <p>I build AI-driven interfaces and systems.</p>
+    `);
+  }
+
+
+  //Clear Command Logic
   else if (command === "clear") { //Opening project windows 
     logs.innerHTML = "";
     disableInput();
     await runInstructSequence();
   }
 
+  //Help Command Logic
+  else if (command === "help") {
+    await typeLog("---------------");
+      addCommandLink("/about");
+      addCommandLink("/skills");
+      addCommandLink("/projects");
+      addCommandLink("/contact");
+      addCommandLink("/clear");
+      addCommandLink("/help")
+    await typeLog("---------------");
+  }
+
+  //Incorrect command
   else {
     await typeLog("unknown command");
   }
+
 }
 
 /* ==============================*/
@@ -329,14 +337,74 @@ input.addEventListener("keydown", (e) => {
   }
 });
 
+
+
+
 /* ==============================*/
-/* Project Logic               */
+/* Project Data                  */
+/* ==============================*/
+
+const projects = {
+  "dream_terminal(placeholder)": {
+    title: "Dream Terminal Portfolio",
+    description: "Terminal Style Portfolio UI",
+    problem: "Traditional Portfolios lack interactivity",
+    approach: "built command-driven HUD interface",
+    results: "Created dynamic terminal system",
+    improvements: "Add persistence animations",
+    tech: ["HTML", "CSS", "JavaScript"],
+    learnings: "UI Architecture, command parsing, UX Systems"
+  }
+};
+
+const aboutData = {
+  intro: [
+    "Place Holder"
+  ],
+
+  education: [
+    {
+      title: "B.S in Computer Science + AI",
+      sub: "Colby College",
+      time: "2022 - 2026",
+      description: "Description Placeholder"
+    }
+  ],
+
+  experience: [
+    {
+      title: "Logistics Coordinator",
+      sub: "Colby College",
+      time: "2024",
+      description: "Description Placeholder"
+    }
+  ],
+
+  current: [
+    {
+      title: "Placeholder Title",
+      sub: "Placeholder Sub",
+      time: "Placeholder of Timeline",
+      description: "Placeholder Description"
+    }
+  ],
+
+  stats: [
+    {value: "3", label: "Projects Completed"},
+    {value: "2+", label: "Years Experience"}
+  ]
+}
+
+
+
+/* ==============================*/
+/* Window Page Creation Logic    */
 /* ==============================*/
 
 //Helper Functions
 function renderBlock(title, content) {
   return `
-    <div class="project-card-block">
+    <div class="card-block">
       <h3>${title}</h3>
       <p>${content}</p>
     </div>
@@ -345,7 +413,7 @@ function renderBlock(title, content) {
 
 function renderList(title, items) {
   return `
-    <div class="project-card-side">
+    <div class="card-block">
       <h3>${title}</h3>
       <ul>
         ${items.map(item => `<li>${item}</li>`).join("")}
@@ -354,31 +422,86 @@ function renderList(title, items) {
   `;
 }
 
+function renderAboutList(title, items) {
+  return `
+    <div class="card-block">
+      <h3>${title}</h3>
+      <div>
+        ${items.map(item => `
+            <div>
+              <h4>${item.title}</h4>
+              <span class="about-sub">${item.sub}</span>
+              <span class="about-time">${item.time}</span>
+              <p>${item.description}</p>
+            </div>
+          `).join("")}
+      </div>
+    </div>
+  `
+}
+
+function renderStats(stats){
+  return stats.map(stat => `
+    <div class="card-block">
+      <strong>${stat.value}</strong>
+      <span>${stat.label}</span>
+    </div>
+  `).join("");
+}
+
+/* ======= */
+/*  About  */
+/* ======= */
+
+function renderAbout(data) {
+  return `
+      <div class="about-main">
+        <h1>About Me</h1>
+        ${data.intro.map(p => `<p>${p}</p>`).join("")}
+      </div>
+
+      <div class="about-column">
+        ${renderAboutList("Education", data.education)}
+        ${renderAboutList("Experience", data.experience)}
+        ${renderAboutList("Current Plans", data.current)}
+      </div>
+
+      <div class="work-column">
+        ${renderStats(data.stats)}
+      </div>
+  `
+}
+
+/* ========= */
+/*  Project  */
+/* ========= */
+
 
 function renderProject(project) {
   return `
-    <div class="project-window">
-      <div class="project-main">
-        <h1>${project.title}</h1>
-        <p>${project.description}</p>
+    
+    <div class="project-main">
+      <h1>${project.title}</h1>
+      <p>${project.description}</p>
+    </div>
+
+    <div class="project-columns">
+      <div class="project-left">
+        ${renderBlock("Problem Statement", project.problem)}
+        ${renderBlock("Technical Approach", project.approach)}
+        ${renderBlock("Results", project.results)}
+        ${renderBlock("Improvements", project.improvements)}
       </div>
 
-      <div class="project-columns">
-        <div class="project-left">
-          ${renderBlock("Problem Statement", project.problem)}
-          ${renderBlock("Technical Approach", project.approach)}
-          ${renderBlock("Results", project.results)}
-          ${renderBlock("Improvements", project.improvements)}
-        </div>
-
-        <div class="project-right">
-          ${renderList("Tech Stack", project.tech)}
-          ${renderBlock("Key Learnings", project.learnings)}
-        </div>
+      <div class="project-right">
+        ${renderList("Tech Stack", project.tech)}
+        ${renderBlock("Key Learnings", project.learnings)}
       </div>
     </div>
+    
   `;
 }
+
 
 
 /* ==============================*/
